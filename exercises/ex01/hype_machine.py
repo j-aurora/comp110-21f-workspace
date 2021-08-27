d77c5b2fae3e3fc9c7af7dbfac3d4b0c2c734302
Aurora Read <jaurora@email.unc.edu>
2021-08-27 11:23:56 -0400

    part 1

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,5 +1,7 @@
-# TODO: Write docstring here
+"""will boost spirits"""
 
-# TODO: Initialize __author__ variable here
+__author__ ="730396458"
 
-# TODO: Implement your program logic here
+name: str = input("What is your name? ")
+print("You entered: ")
+print(name)
