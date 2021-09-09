10a7b058407c830b16b7c190fafd83cc270b12be
Aurora Read <jaurora@email.unc.edu>
2021-09-08 23:33:48 -0400

    completed

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -1,6 +1,18 @@
 """Counting letters in a string."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730396458"
 
 
-# Begin your solution here...
+letter: str = input("What letter do you want to search for?: ")
+word: str = input("Enter a word: ")
+count: int = 0
+i: int = len(word) - 1 
+
+while i >= 0:
+    if word[i] == letter:
+        count = count + 1
+        i = i - 1
+    else:
+        i = i - 1
+
+print("Count: " + str(count)) 
