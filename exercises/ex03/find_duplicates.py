cc8ff3ad43ae8430be8a48469f0355dc92180142
Aurora Read <jaurora@email.unc.edu>
2021-09-15 23:16:28 -0400

    allattempted

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,19 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730396458"
+
+word: str = input("Enter a word: ")
+answer: str = ""
+i: int = len(word) - 1
+p: int = len(word) - 1
+
+while i >= 0:
+    while p >= 0:
+        if word[i] == word[p]:
+            answer = "True"
+        else: 
+            answer = "False"
+        p = p - 1
+    i = i - 1
+
+print("Found duplicate: " + answer)
--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -1,6 +1,14 @@
 """Drawing forests in a loop."""
 
-__author__ = "123456789"
+__author__ = "730396458"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+stack: int = int(input("Depth: "))
+i: int = 0
+tree_line: str = ""
+
+while i < stack:
+    tree_line = tree_line + TREE
+    print(tree_line)
+    i = i + 1
