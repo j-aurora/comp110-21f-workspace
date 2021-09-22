e4e4574efdc85ca11e551b0dec6b4e53c12d0204
Aurora Read <jaurora@email.unc.edu>
2021-09-22 09:56:45 -0400

    started

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -8,12 +8,12 @@ i: int = len(word) - 1
 p: int = len(word) - 1
 
 while i >= 0:
-    while p >= 0:
+    if p != i:
         if word[i] == word[p]:
             answer = "True"
-        else: 
+    
+        else:
             answer = "False"
+        print("Found duplicate: " + answer)
         p = p - 1
     i = i - 1
-
-print("Found duplicate: " + answer)
---
+++ projects/cyoa.py
@@ -0,0 +1,12 @@
+"""Create an adventure, what type pf noodle are you."""
+
+
+points: int = 0
+player: str = ""
+
+def greet(): 
+    print("Welcome to this little quiz. ")
+    player = input("What would you like to be called? ")
+    return None 
+
+input("Are you ready to begin? ")
