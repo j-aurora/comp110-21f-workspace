cbc09f9e0570df3c5efa0700bf3f83c864969a4c
Aurora Read <jaurora@email.unc.edu>
2021-09-22 20:32:50 -0400

    post lesson

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -1,12 +1,27 @@
-"""Create an adventure, what type pf noodle are you."""
-
+"""Create an adventure, what type of houseplant are you."""
 
 points: int = 0
 player: str = ""
 
-def greet(): 
+
+def main() -> None:
+    greet()
+
+
+def greet() -> None: 
+    """Greet the player.""" 
     print("Welcome to this little quiz. ")
     player = input("What would you like to be called? ")
-    return None 
+    print("Do want to continue?" + player)
+    choice_1: str = input("(Enter No, Yes, or Tell me more) ")
+    if choice_1 == "No":
+        print("I'm sorry to see you go." )
+        print(points)
 
-input("Are you ready to begin? ")
+
+def Que1(x: str) -> str:
+
+
+
+if __name__ == "__main__":
+    main()
