f3053e67dcdabc45679f310fb11b4808df33d0bd
Aurora Read <jaurora@email.unc.edu>
2021-10-20 22:47:26 -0400

    firstsub

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -3,3 +3,67 @@
 __author__ = "730396458"
 
 # Define your functions below
+
+
+def count(start: list[str]) -> dict[str, int]:
+    """Create dictionary where value is the number of times the key appears in a list. """
+    name_count: dict[str, int] = {}
+    i: int = 0
+    while i < len(start):
+        name_count[start[i]] = 0
+        i += 1
+    runthru: int = 0
+    while runthru < len(start):
+        if start[runthru] in name_count:
+            name_count[start[runthru]] += 1 
+        runthru += 1
+    return name_count
+
+
+def invert(a: dict[str, str]) -> dict[str, str]:
+    """Returns a dictionary inverting keys and values."""
+    val_list: list[str] = []
+    key_list: list[str] = [] 
+    for key in a:
+        key_list.append(key)
+        val_list.append(a[key])
+    proof: dict[str, int] = count(val_list)
+    for key in proof:
+        if proof[key] > 1:
+            raise KeyError("A repeated key is invalid.")
+    result: dict[str, str] = {}
+    i: int = 0
+    while i < len(val_list):
+        result[val_list[i]] = key_list[i]
+        i += 1
+    return result
+
+# It returns a str which is the color that appears most frequently.
+# If there is a tie for most popular color, return the color that appeared in the dictionary first.
+
+
+def favorite_color(colors: dict[str, str]) -> str:
+    """Return the most common favorite color."""
+    colchoice: list[str] = []
+    for key in colors:
+        colchoice.append(colors[key])
+    colorcho: dict[str, int] = count(colchoice)
+    val_list: list[int] = []
+    key_list: list[str] = [] 
+    for key in colorcho:
+        key_list.append(key)
+        val_list.append(colorcho[key])
+    values: int = len(val_list) - 1
+    max_value: int = val_list[values]
+    while values > 0:
+        if max_value < val_list[values - 1]:
+            max_value = val_list[values - 1]
+        values -= 1
+    result: dict[int, str] = {}
+    i: int = 0
+    while i < len(val_list):
+        result[val_list[i]] = key_list[i]
+        i += 1
+    return result[max_value] 
+    
+    
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,56 @@
 """Unit tests for dictionary functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+__author__ = "730396458"
+
+
+def test_invert_edge() -> None:
+    """Use an empty dictionary."""
+    totest: dict[str, str] = {}
+    assert invert(totest) == {}
+
+
+def test_invert_repeat() -> None:
+    """Have a repeated key."""
+
+
+def test_invert_use() -> None:
+    "A normal use with a multi key dictionary."
+    totest: dict[str, str] = {'a': 'b', 'c': 'd', 'e': 'f'}
+    assert invert(totest) == {'b': 'a', 'd': 'c', 'f': 'e'}
+
+
+def test_fav_edge() -> None:
+    """The edge case."""
+    start: dict[str, str] = {}
+    assert favorite_color(start) == {}
+ 
+
+def test_fav_many() -> None:
+    """A different use case with many choices."""
+    choices: dict[str, str] = {'Kay': 'blue', 'Greg': 'blue', 'Kate': "orange", "Meg": 'purple'}
+    assert favorite_color(choices) == 'blue'
+    
+
+def test_fav_use() -> None:
+    """A use case with few choices."""
+    choices: dict[str, str] = {'Mike': 'grey', 'Meg': 'blue'}
+    assert favorite_color(choices) == 'grey'
+
+
+def test_count_edge() -> None:
+    """Edge case for count."""
+    new_list: list[str] = []
+    assert count(new_list) == {}
+
+
+def test_count_use() -> None:
+    one: list[str] = ['h', 'e', 'l', 'l', 'o', 'o']
+    assert count(one) == {'h': 1, 'e': 1, 'l': 2, 'o': 2}
+
+
+def test_count_single() -> None:
+    one: list[str] = ["lol"]
+    assert count(one) == {"lol": 1}
