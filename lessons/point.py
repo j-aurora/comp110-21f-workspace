e4bd46514d15599f0d2a283972654f6242f26cce
Aurora Read <jaurora@email.unc.edu>
2021-11-10 15:21:01 -0500

    lesson

--- lessons/point.py
+++ lessons/point.py
@@ -18,13 +18,50 @@ class Point:
 
     def scale(self, factor: float) -> Point:
         """Immutable: multiplies components by factor without mutation."""
-        x: float = self.x * factor
-        y: float = self.y * factor
-        scaled_point: Point = Point(x, y)
-        return scaled_point
+        return Point(self.x * factor, self.y * factor)
 
+    def __mul__(self, factor: float) -> Point:
+        """Overload the multiplication operator."""
+        print("__mul__ was called.")
+        return Point(self.x * factor, self.y * factor)
+
+    def __str__(self) -> str:
+        """Produce a str representation of a Point for humans."""
+        return f"({self.x}, {self.y})"
+
+    def __repr__(self) -> str:
+        """Produce a str representation of a Point for Python."""
+        return f"Point({self.x}, {self.y})"
+
+    def __add__(self, rhs: Point) -> Point:
+        """Overload additon operator."""
+        print("__add_ was called")
+        return Point(self.x + rhs.x, self.y + rhs.y)
+
+    def __getitem__(self, index: int) -> float:
+        """Apply index to the point."""
+        if index == 0:
+            return self.x
+        elif index == 1:
+            return self.y
+        else: 
+            raise IndexError
+
+a: Point = Point(1.0, 2.0)
+b: Point = a * 2.0
+c: Point = a + b
+print(f"a: {a}")
+print(f"b: {b}")
+print(f"c: {c}")
+
+print(a[0])
+print(a[1])
 
 p0: Point = Point(1.0, 2.0)
 p1: Point = p0.scale(2.0)
-print(f"{p0.x}, {p0.y}")
-print(f"({p1.x}, {p1.y})")
+print(p0)
+pl_as_a_str: str = str(p1)
+print(pl_as_a_str)
+
+p1_repr: str = repr(p1)
+print(p1_repr)
